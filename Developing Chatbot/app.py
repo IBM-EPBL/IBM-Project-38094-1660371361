b21cb6ce409ae216a1e38ac58e9ab41e19d15723
yashwanth1135 <100790850+yashwanth1135@users.noreply.github.com>
2022-11-20 19:29:51 +0530

    Add files via upload

---
+++ Developing Chatbot/app.py
@@ -0,0 +1,22 @@
+from flask import Flask , render_template , request , redirect , url_for
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
